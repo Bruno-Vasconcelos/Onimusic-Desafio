275e5fce3c9b1f4d2b42dc34a9dd40d2934c7cc8
Bruno <brunovictorvasconcelos@gmail.com>
2021-02-26 11:23:16 -0300

    Primeiro Commit

---
+++ onimusicdesafio/musiconi/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MusiconiConfig(AppConfig):
+    name = 'musiconi'
+
---
+++ onimusicdesafio/musiconi/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Song(models.Model):
+    title = models.CharField(max_length=50)
+    duration = models.IntegerField()
+    spotify_published = models.BooleanField()
+    youtube_published = models.BooleanField()
+    artist = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+class Artist(models.Model):
+    name = models.CharField(max_length=30)
+    date_joined = models.DateField()
+
+
