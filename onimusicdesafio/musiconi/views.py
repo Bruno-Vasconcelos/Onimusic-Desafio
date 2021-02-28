bccbf80ed6b00cd87de7145b7aff2ae788a230a4
Bruno <brunovictorvasconcelos@gmail.com>
2021-02-28 13:35:21 -0300

    modificaçoes finais

--- onimusicdesafio/musiconi/views.py
+++ onimusicdesafio/musiconi/views.py
@@ -7,12 +7,14 @@ from django.http import HttpResponse
 def home(request):
     songs = Song.objects.all()
     total_songs = songs.count()
+    
     context = {'songs':songs}
     return render(request, 'musiconi/home.html', context)
 
 
 def createSong(request):
     form = SongForm()
+
     if request.method == 'POST':
         form = SongForm(request.POST)
         if form.is_valid():
@@ -22,8 +24,8 @@ def createSong(request):
     context = {'form':form}
     return render(request,'musiconi/song_form.html', context)
 
+
 def updateSong(request, pk):
-    
     song = Song.objects.get(id=pk)
     form = SongForm(instance=song)
     
@@ -36,10 +38,14 @@ def updateSong(request, pk):
     context = {'form':form}
     return render(request,'musiconi/song_form.html', context)
 
+
+
 def deleteSong(request,pk):
     song = Song.objects.get(id=pk)
+
     if request.method == 'POST':
         song.delete()
         return redirect('/')
+
     context = {'item':song}
     return render(request,'musiconi/delete.html', context)
